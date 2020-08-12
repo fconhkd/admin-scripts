9408bddf8f4d26061e8de905afea6dd941b8e906
Fabiano Conrado <fabianoconrado@gmail.com>
2020-08-12 19:59:26 -0300

    Ajustando a rotina de backup do PostgreSQL para incluir a hora do backup

--- db/postgres/backupPSQL.bat
+++ db/postgres/backupPSQL.bat
@@ -8,27 +8,25 @@ for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
 	set year=%%k
 )
 
+:: get time
+for /F "tokens=1-2 delims=:. " %%i in ('time /t') do (
+	set hh=%%i
+	set mi=%%j
+)
+
+
 set datestr=%day%-%month%-%year%
 echo datestr is %datestr%
 
 rem SET PGPASSWORD=<PassWord>
+SET DATABASE_NAME=<DatabaseAqui>
+SET SERVER_IP=10.20.10.250
 
 echo off
-set BACKUP_FILE=%datestr%_cartago_fiscal_log.backup
+set BACKUP_FILE=%datestr%_%hh%%mi%_%DATABASE_NAME%.backup
 echo backup file name is %BACKUP_FILE%
 echo on
-bin\pg_dump.exe -i -h estacao011 -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% cartago_fiscal_log
+bin\pg_dump.exe -i -h %SERVER_IP% -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% %DATABASE_NAME%
 
-echo off
-set BACKUP_FILE=%datestr%_cartago_fiscal_rdc.backup
-echo backup file name is %BACKUP_FILE%
-echo on
-bin\pg_dump.exe -i -h estacao012 -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% cartago_fiscal_rdc
-
-echo off
-set BACKUP_FILE=%datestr%_cartago_fiscal_drj.backup
-echo backup file name is %BACKUP_FILE%
-echo on
-bin\pg_dump.exe -i -h estacao013 -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% cartago_fiscal_drj
 
 :FFIM

b8434fcfcff0764b136329b6df243914e86d0fac
Fabiano Conrado <fabianoconrado@gmail.com>
2021-01-05 18:40:01 -0300

    Incluindo novos arquivos

---
+++ tools/alterar_upn_suffixDomain.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module ActiveDirectory
+
+#Esse é o antigo sufixo de dominio
+$oldSuffix = "domain.corp"
+
+#Esse é o novo sufixo
+$newSuffix = "domain.com.br"
+
+#Unidade Organizacional onde faremos as alterações
+$OU = "OU=MYOU,DC=domain,DC=corp"
+
+#nome do servidor DC
+$server = "nameServer"
+
+Get-ADUser -SearchBase $OU -Filter * | ForEach-Object {
+    $newUPN = $_.UserPrincipalName.Replace($oldSuffix,$newSuffix)
+    $_ | Set-ADUser -Server $server -UserPrincipalName $newUPN
+}

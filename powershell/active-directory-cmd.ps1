2a517c69b96afad2e719a75d7fe1b843bda9c768
Fabiano Conrado de Oliveira <fabiano.conrado@grupocard.com.br>
2021-05-28 16:20:52 -0300

    novos comandos

--- powershell/active-directory-cmd.ps1
+++ powershell/active-directory-cmd.ps1
@@ -2,10 +2,13 @@
 $secpass = Read-Host "Password" -AsSecureString    
 
 //criar um password usando um texto plano
-$secpass = ConvertTo-SecureString "123@Mudar" -asPlainText -Force
+$secpass = ConvertTo-SecureString "123@Mudar" -AsPlainText -Force
 
 //mudar a senha do usuario
 Set-ADAccountPassword fabianohkd -Reset -NewPassword $secpass
+
+//o mesmo de cima em uma unica linha
+Set-ADAccountPassword -Identity fabianohkd -Reset -NewPassword (ConvertTo-SecureString -AsPlainText "$newPass" -Force)
 
 //modificar propriedades em usuários
 Set-ADUser fabianohkd -replace @{Mobile="12997872366"}
